04f00b68f6d74a3271290c9382cb21408119c5f0
edielam <edguard738@gmail.com>
2022-03-09 02:22:15 +0000

    adding entities

---
+++ flightservices/src/main/java/com/flight/flightservices/entities/Passenger.java
@@ -0,0 +1,59 @@
+package com.flight.flightservices.entities;
+
+public class Passenger {
+    private int id;
+    private String firstname;
+    private String lastName;
+    private String middleName;
+    private String email;
+    private String phone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+}
